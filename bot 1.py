eda561accd81d985fec965951d70f571d80c1952
Preetish12345 <110787990+Preetish12345@users.noreply.github.com>
2022-08-08 02:31:39 +0530

    Add files via upload

---
+++ bot 1.py
@@ -0,0 +1,43 @@
+import time
+import keyboard
+from time import time,ctime
+import os
+
+def clear():
+    os.system('cls')
+
+clear()
+
+X = []
+Y = []
+
+n = 0
+b = 0
+
+if keyboard.is_pressed('q') == False: 
+    t = time()   
+    while 1:
+        while keyboard.is_pressed('right arrow') ==True:
+            a = time()
+            c = a-b
+            if c<10000:
+                Y.append(c)
+            print('Y ',Y)
+            while 1:
+                if keyboard.is_pressed('right arrow') == False:
+                    b = time()
+                    c = b-a
+                    X.append(c)
+                    print('X ',X)
+                    break
+
+#        while keyboard.is_pressed('right arrow') ==False:
+#            a = time()
+#            while 1:
+#                if keyboard.is_pressed('right arrow') == True:
+#                    b = time()
+#                    c = b-a
+#                    Y.append(c)
+#                    print(Y)
+#                    break
+
