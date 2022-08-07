c477bb80a0615c15b71eae655beea1107dd99938
Preetish12345 <110787990+Preetish12345@users.noreply.github.com>
2022-08-08 03:37:45 +0530

    Update bot 1.py

--- bot 1.py
+++ bot 1.py
@@ -3,32 +3,28 @@ import keyboard
 from time import time,ctime
 import os
 
-def clear():
-    os.system('cls')
-
-clear()
+os.system('cls')
 
 X = []
 Y = []
 
+t = time()   
+while 1:
+    while keyboard.is_pressed('left arrow') ==True:
+        a = time()
+        c = a-t
+        Y.append(c)
+        print('Y ',Y)
+        while 1:
+            if keyboard.is_pressed('left arrow') == False:
+                b = time()
+                c = b-t
+                X.append(c)
+                print('X ',X)
+                break
+            if keyboard.is_pressed('q') == True:
+                break                     
+    if keyboard.is_pressed('q') == True:
+        break   
 
-if keyboard.is_pressed('q') == False: 
-    t = time()   
-    while 1:
-        while keyboard.is_pressed('left arrow') ==True:
-            a = time()
-            c = a-t
-            Y.append(c)
-            print('Y ',Y)
-            while 1:
-                if keyboard.is_pressed('left arrow') == False:
-                    b = time()
-                    c = b-t
-                    X.append(c)
-                    print('X ',X)
-                    break
-                if keyboard.is_pressed('q') == True:
-                    break                     
-        if keyboard.is_pressed('q') == True:
-            break   
 
