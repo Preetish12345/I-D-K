b991a4359b48910066b0f8654f017a2882c94427
Preetish12345 <110787990+Preetish12345@users.noreply.github.com>
2022-08-08 03:35:12 +0530

    Update bot 1.py

--- bot 1.py
+++ bot 1.py
@@ -11,32 +11,24 @@ clear()
 X = []
 Y = []
 
-b = 0
 
 if keyboard.is_pressed('q') == False: 
     t = time()   
     while 1:
-        while keyboard.is_pressed('right arrow') ==True:
+        while keyboard.is_pressed('left arrow') ==True:
             a = time()
             c = a-t
             Y.append(c)
             print('Y ',Y)
             while 1:
-                if keyboard.is_pressed('right arrow') == False:
+                if keyboard.is_pressed('left arrow') == False:
                     b = time()
                     c = b-t
                     X.append(c)
                     print('X ',X)
                     break
+                if keyboard.is_pressed('q') == True:
+                    break                     
+        if keyboard.is_pressed('q') == True:
+            break   
 
-#        while keyboard.is_pressed('right arrow') ==False:
-#            a = time()
-#            while 1:
-#                if keyboard.is_pressed('right arrow') == True:
-#                    b = time()
-#                    c = b-a
-#                    Y.append(c)
-#                    print(Y)
-#                    break
-
-# editor
